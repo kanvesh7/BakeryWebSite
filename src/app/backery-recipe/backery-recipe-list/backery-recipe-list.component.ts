0f7cf6a2245a4ba65e99bdc6571c3a73a09a7fb0
Anvesh Kodumuri <k.anvesh@hotmail.com>
2021-02-16 19:56:46 -0500

    master

--- src/app/backery-recipe/backery-recipe-list/backery-recipe-list.component.ts
+++ src/app/backery-recipe/backery-recipe-list/backery-recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BakeryRecipe } from '../bakeryRecipe.model';
 
 @Component({
   selector: 'app-backery-recipe-list',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./backery-recipe-list.component.css']
 })
 export class BackeryRecipeListComponent implements OnInit {
+  
+  bakeryRecipeLists: BakeryRecipe[] = [
+    new BakeryRecipe("Cinnamon Roll", "This is Cinnamon Roll", "../../../assets/images/Cinnamon.jpg"),
+    new BakeryRecipe("Banana Bread", "This is Banana Bread", "../../../assets/images/bananaBread.jpg"),
+    new BakeryRecipe("Chocolate Pastry", "this is a chocklate pastry", "../../../assets/images/ChocolatePastry.jpg")
+  ];
 
-  constructor() { }
+  constructor() { 
+  }
 
   ngOnInit(): void {
   }
+
+ 
 
 }
